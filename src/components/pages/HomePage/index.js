9530eddeebfbc93a84af29b8d20bb3603773c123
Frank Yandolino <frank.yandolino@allergan.com>
2018-11-11 18:40:40 -0500

    [homepage] - initial commit

--- src/components/pages/HomePage/index.js
+++ src/components/pages/HomePage/index.js
@@ -1,14 +1,31 @@
 import React from 'react'
 
-import { PageTemplate, Link, Button } from 'components'
+import { PageTemplate, Link, Button, Heading, Row, Column  } from 'components'
+
+import './home.styl'
 
 const HomePage = () => {
   return (
     <PageTemplate name="home">
       <div className="container">
-        <p>Im the homepage.</p>
-        <p>Here is a link <Link href="http://google.com" target="_blank">Google</Link></p>
-        <Button className="btn btn-primary">fff</Button>
+        <Row className="mt-5 pt-4">
+          <Column
+            md={{ span: 6 }}
+            className="hp-hero"
+          >
+            <Heading level={2} className="page-hero-title">I Need a Full-Service Production Team!</Heading>
+            <p className="mb-0">"Let’s custom-build a dream team of Cut & Post creative professionals to manage your video project from concept to completion."</p>
+            <Button type="submit" className="btn btn-primary submit-btn">Cut & Post agency</Button>
+          </Column>
+          <Column
+            md={{ span: 6 }}
+            className="hp-hero"
+          >
+            <Heading level={2} className="page-hero-title">I Want to Hire Individual Freelancers!</Heading>
+            <p className="mb-0">"A diverse talent pool of award winning producers, editors, graphics artists and creative professionals, ready to work on-set, in-house, or remotely."</p>
+            <Button type="submit" className="btn btn-primary submit-btn">Cut & Post staffing</Button>
+          </Column>
+        </Row>
       </div>
     </PageTemplate>
   )
